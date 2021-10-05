02f017ee22123ba23d16c9f31c1d45d2ab06281a
Joaquin Sopena <696272@unizar.es>
2021-10-05 11:16:54 +0200

    Deleted adc conversion to volts

--- ejc1/ejc1.ino
+++ ejc1/ejc1.ino
@@ -3,7 +3,6 @@
 
 const int aPin = 36; //ESP32-DevKit ADC1_CH0
 int value = 0;      //Value read from ADC1_CH0
-float value_volts = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -11,9 +10,7 @@ void setup() {
 
 void loop() {
   value = analogRead(aPin); 
-  value_volts = (value * 3.3)/4095;   //Vref = 3.3V, ADC -> 12 bits
   Serial.println(value);
-  Serial.println(value_volts);
   delay(1000);
 
 }
